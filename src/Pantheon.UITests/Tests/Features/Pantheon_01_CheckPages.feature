0318102c531fa73283e95dd13e595693200ad4dc
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-09-14 11:49:49 +0100

    added pantheon tests

---
+++ src/Pantheon.UITests/Tests/Features/Pantheon_01_CheckPages.feature
@@ -0,0 +1,5 @@
+﻿Feature: Pantheon_01_CheckPages
+
+@regression
+Scenario: Pantheon_01_CheckPages
+	Then the sub-links under About menu should be working
---
+++ src/Pantheon.UITests/Tests/StepDefinitions/Steps.cs
@@ -0,0 +1,20 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Pantheon.UITests.Tests.StepDefinitions
+{
+    [Binding]
+    public class Steps
+    {
+        public Steps(ScenarioContext context)
+        {
+
+        }
+
+        [Then(@"the sub-links under About menu should be working")]
+        public void ThenTheSub_LinksUnderAboutMenuShouldBeWorking()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
